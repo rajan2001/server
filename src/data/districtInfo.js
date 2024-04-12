0524d5722df3ded4659f0b24a25aee9b15b7a955
unknown <rajanpandey865@gmail.com>
2024-04-12 10:02:53 +0530

    update

---
+++ src/data/districtInfo.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: "1",
+    DistrictInfo: {
+      EcologicalZone:
+        "In this district, the Agro Ecological Sub Region (ICAR) is designated as Moderately To Gently Sloping Chattisgarh-Mahanadi Basin, falling under the Hot Moist/Dry Subhumid Transitional eco sub-region (11.0). It is also categorized within the Agro-Climatic Zone (Planning Commission) as the Eastern Plateau And Hills Region (VII), and under the Agro Climatic Zone (NARP) it belongs to the Central and North Eastern Plateau Zone (BI-4)",
+      Coordinates:
+        "The district headquarters is situated at a latitude of 23.35°N and a longitude of 85.33°E, with an altitude of 2140 feet.",
+    },
+    Seasons: {
+      season1: "Kharif- Rainfed",
+      season1: "Kharif-Irrigated",
+    },
+    crop: "Rice",
+    city:'RAMGARH'
+  },
+  
+];
--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
 const express = require("express");
 
-const state = require("./data/state")
-const district = require("./data/district")
-const crops = require("./data/crops")
-const bodyParser = require('body-parser');
-const cors = require("cors")
+const state = require("./data/state");
+const district = require("./data/district");
+const crops = require("./data/crops");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const districtInfo = require("./data/districtInfo");
 
 const app = express();
 const Port = 3001;
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/states", (req, res) => {
   res.json(state);
 });
 
-app.get('/states/cities',(req,res)=>{
-    const searchTerm = req.query.name;
-    const city = district.filter((city)=>city.state === searchTerm)
-    res.send(city)
-})
-
-app.get('/cities/crops',(req,res)=>{
+app.get("/states/cities", (req, res) => {
   const searchTerm = req.query.name;
-  const crop = crops.filter((crop)=>crop.city === searchTerm)
-  res.send(crop)
-})
+  const city = district.filter((city) => city.state === searchTerm);
+  res.send(city);
+});
+
+app.get("/cities/crops", (req, res) => {
+  const searchTerm = req.query.name;
+  const crop = crops.filter((crop) => crop.city === searchTerm);
+  res.send(crop);
+});
+
+app.get("/cities/crops/info", (req, res) => {
+  const searchTerm1 = req.query.crop;
+  const searchTerm2 = req.query.city;
+
+  const information = districtInfo.filter(
+    (info) => info.crop === searchTerm1 && info.city === searchTerm2
+  );
+
+  res.send(information)
+});
 
 app.listen(Port, () => {
   console.log(`listen on Port ${Port}`);
